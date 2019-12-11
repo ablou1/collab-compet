cc62abe3e3c610992529f33dc10f2b1f0b37a3f1
Alexandre Blouin <alexandre.blouin@maplink.global>
2019-12-11 08:49:04 +0100

    Add comments

--- parameters.py
+++ parameters.py
@@ -4,16 +4,17 @@ from noise import GaussianNoise
 class AgentParameters():
     """Contains the parameters to apply to the agent"""
 
-    def __init__(self, action_size, gamma=0.99, tau_param=(0.01, 0.001, 0.999), batch_size=1024, learn_param=(20, 20), noise_factor=0.4, noise_reducer_param=(1, 0.01, 0.9995), model_update=(20, 20), model_param=(256, 512), lr_actor_critic=(1e-4, 1e-4)):
+    def __init__(self, action_size, gamma=0.99, tau_param=(0.01, 0.001, 0.999), batch_size=1024, learn_param=(20, 20), noise_factor=0.4, noise_reducer_param=(1, 0.01, 0.9995), model_update=(1, 1), model_param=(256, 512), lr_actor_critic=(1e-4, 1e-4)):
         """Initialize agent parameters.
         Params
         ======
             gamma (float): discount factor
             tau_param (float, float, float): for soft update of target parameters (factor, min_factor, decay_rate)
             batch_size (int): minibatch size
-            learn_param (int, int)
-            noise_param(float, float, float): for noise applied in act method (factor, min_factor, decay_rate)
-            model_update(int, int): tuple that contains the number of iterations without training and the number of training done
+            learn_param (int, int): tuple that contains the number of iterations without training and the number of training done each time
+            noise_factor: factor apply to the Gaussian noise
+            noise_reducer_param(float, float, float): for noise applied in act method (factor, min_factor, decay_rate)
+            model_update(int, int): tuple that contains the number of iterations without training and the number of training done each time
             model_param(int, int): tuple of hidden layer size for the actor and critic network
             lr_actor_critic (float, float): tuple of learning rates of the actor and of the critic
         """
